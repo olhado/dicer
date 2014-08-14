ba7a77e32263ea5e2b82c698ae7b97bae96bf412
Michael Chmielewski <code@mikec.123mail.org>
2014-08-13 21:46:44 -0400

    Refactored validation
    * Checks for invalid characters
    * Makes sure parentheses are balanced
    * Makes sure parentheses include terms
    
    Added .gitignore file

--- dice.ex
+++ dice.ex
@@ -1,28 +1,74 @@
-defmodule Dice2 do
+defmodule Dice do
   defmodule Scalar_Term, do: defstruct operand: nil, value: 0
   defmodule Die_Term, do: defstruct operand: nil, quantity: 0, sides: 10
   defmodule Complex_Term, do: defstruct operand: nil, terms: []
 
   def parse(dice_str) when is_binary(dice_str) do
-    parsed_dice =
-      case  _validate(dice_str) do
-        {:ok, validated_dice_str} ->
-          IO.inspect _parse(validated_dice_str)
-          IO.puts validated_dice_str
-#      {:ok, validated_dice_str} -> IO.puts "Valid!!!"
+      stripped_str = _strip_spaces(dice_str)
+
+      case _validate(stripped_str) do
+        {:ok, []} ->
+          IO.inspect _parse(stripped_str)
+          IO.puts stripped_str
         {:error, reason} ->
           IO.puts reason
+        _ ->
+          {:error, ["Unknown error!!!\n"]}
       end
   end
 
   defp _validate(dice_str) do
-    stripped_str = _strip_spaces(dice_str)
-    valid_str = Regex.match?(~r|^[0-9d\-\+\*\/\(\)]+$|i, stripped_str)
+    {:ok, []}
+    |> _valid_chars?(dice_str)
+    |> _correct_num_of_parens?(dice_str)
+    |> _empty_parens_clauses?(dice_str)
+
+  end
+
+  defp _valid_chars?(validation_tuple = {_, messages}, dice_str) do
+    valid_str = Regex.match?(~r/^[0-9d\-\+\*\/\(\)]+$/i, dice_str)
 
     cond do
-      !valid_str  -> {:error, "Dice string has invalid characters!"} 
-      valid_str   -> {:ok, dice_str}
+      !valid_str  -> {:error, ["Dice string has invalid characters!\n" | messages]}
+      valid_str   -> validation_tuple
     end
+  end
+
+  defp _correct_num_of_parens?(validation_tuple = {_, messages}, dice_str) do
+    parens_count = _count_parens(dice_str, 0)
+
+    if parens_count != 0 do
+      {:error, ["Parentheses do not match!\n" | messages]}
+    else
+      validation_tuple
+    end
+  end
+
+  defp _empty_parens_clauses?(validation_tuple = {_, messages}, dice_str) do
+    match = Regex.match?(~r/\(\)/i, dice_str)
+
+    if match do
+      {:error, ["Empty clause in input!\n" | messages]}
+    else
+      validation_tuple
+    end
+  end
+
+  defp _count_parens("", parens_count) do
+    parens_count
+  end
+
+  defp _count_parens(dice_str, parens_count) do
+    char = String.at(dice_str, 0)
+    case char do
+      "("   -> _count_parens(String.slice(dice_str, 1..-1), parens_count + 1)
+      ")"   -> _count_parens(String.slice(dice_str, 1..-1), parens_count - 1)
+      _     -> _count_parens(String.slice(dice_str, 1..-1), parens_count)
+    end
+  end
+
+  defp _check_for_empty_parens_clauses() do
+    
   end
 
   defp _strip_spaces(dice_str) do
@@ -46,7 +92,7 @@ defmodule Dice2 do
       
       # Die terms - A single die
       match = Regex.run(~r/^([\+\-\*\/]?)d(\d+)(.*)/i, dice_str) ->
-        [_, operand, _quantity, sides, rest] = match
+        [_, operand, sides, rest] = match
         IO.inspect match
         IO.inspect rest
         {num_of_sides, _} = Integer.parse(sides)
