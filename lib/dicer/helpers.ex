ec12c40979cb6e49ea7b50ee13128c7e5a345bd9
Michael Chmielewski <code@mikec.123mail.org>
2014-10-01 00:01:54 -0400

    Removed old unneeded files

--- lib/dicer/helpers.ex
+++
@@ -1,9 +0,0 @@
-defmodule Dicer.Helpers do
-  def determine_operand("") do
-    "+"
-  end
-
-  def determine_operand(operand) when is_binary(operand) and byte_size(operand) == 1 do
-    operand
-  end
-end
