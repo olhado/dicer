03753e0b71a0be9fe3d2f360a08e21b70f45d811
Michael Chmielewski <code@mikec.123mail.org>
2014-10-01 23:45:43 -0400

    Look, unit tests! And they all pass

--- test/dicer_test.exs
+++ test/dicer_test.exs
@@ -4,4 +4,32 @@ defmodule DicerTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "blank input" do
+    assert Dicer.roll("") == {[%Dicer.Tokens.End{}], 0.0}
+  end
+
+  test "single integer" do
+    assert Dicer.roll("1") == {[%Dicer.Tokens.End{}], 1.0}
+  end
+
+  test "single float" do
+    assert Dicer.roll("1.23456789") == {[%Dicer.Tokens.End{}], 1.2346}
+  end
+
+  test "addition" do
+    assert Dicer.roll("1+0.5") == {[%Dicer.Tokens.End{}], 1.5}
+  end
+
+  test "subtraction" do
+    assert Dicer.roll("1-0.5") == {[%Dicer.Tokens.End{}], 0.5}
+  end
+
+  test "multiplication" do
+    assert Dicer.roll("1*0.5") == {[%Dicer.Tokens.End{}], 0.5}
+  end
+
+  test "division" do
+    assert Dicer.roll("1/0.5") == {[%Dicer.Tokens.End{}], 2.0}
+  end
 end
