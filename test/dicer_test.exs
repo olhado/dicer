717b426af46a93b8916afbf5b8abbd8be1dfb32b
Michael Chmielewski <code@mikec.123mail.org>
2014-10-02 20:57:30 -0400

    Another test

--- test/dicer_test.exs
+++ test/dicer_test.exs
@@ -41,12 +41,8 @@ defmodule DicerTest do
     assert Dicer.roll("(1.2222)") == {[%Dicer.Tokens.End{}], 1.2222}
   end
 
-  test "nested parentheses" do
-    assert Dicer.roll("1-(6-((12)+400))") == {[%Dicer.Tokens.End{}], 407.0}
-  end
-
   test "dice roll results stay in expected range" do
-    for _ <- 1..1000 do
+    for _ <- 1..100 do
       {end_token, value} = Dicer.roll("2d20")
       # IO.puts value # TODO: Replace with Logger!
       assert end_token == [%Dicer.Tokens.End{}]
@@ -54,4 +50,11 @@ defmodule DicerTest do
     end
   end
 
+  test "nested parentheses" do
+    assert Dicer.roll("1-(6-((12)+400))") == {[%Dicer.Tokens.End{}], 407.0}
+  end
+
+  test "all operators together" do
+    assert Dicer.roll("1000-350/2*3+(100/(20*5))-575+100d1") == {[%Dicer.Tokens.End{}], 1.0}
+  end
 end
