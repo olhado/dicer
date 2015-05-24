b5c4d914f5043d8dd5527810fbff933542679c7d
Michael Chmielewski <code@mikec.123mail.org>
2015-05-23 21:36:03 -0400

    Modified timing tests

--- lib/timings.ex
+++ lib/timings.ex
@@ -1,11 +1,11 @@
 defmodule Dicer.Timings do
 
   def time() do
-    {time, _} = :timer.tc(Dicer.Timings, :same_string_compute, [1])
+    {time, _} = :timer.tc(Dicer.Timings, :same_string_compute, [100])
     IO.puts time
-    {time, _} = :timer.tc(Dicer.Timings, :with_dice, [1])
+    {time, _} = :timer.tc(Dicer.Timings, :with_dice, [100])
     IO.puts time
-    {time, _} = :timer.tc(Dicer.Timings, :simple, [1])
+    {time, _} = :timer.tc(Dicer.Timings, :simple, [100])
     IO.puts time
   end
 
@@ -16,7 +16,7 @@ defmodule Dicer.Timings do
   end
   def same_string_compute(times) do
     for _x <- 1..times do
-      Dicer.roll("(1-(33+(44-(3.45+(1.23+(8.1-(0.5)))))))")
+      Dicer.roll("(1-(33+(44-(3.45+(1.23+(8.1-(0.5)))))))+(1-(33+(44-(3.45+(1.23+(8.1-(0.5)))))))+(1-(33+(44-(3.45+(1.23+(8.1-(0.5)))))))")
     end
   end
   def with_dice(times) do
